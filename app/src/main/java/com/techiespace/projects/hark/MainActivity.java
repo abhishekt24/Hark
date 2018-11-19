f91f2f337ed95aaafef2ace9073702f0b7b75ca1
techiespace <shreyas.techiespace@gmail.com>
2018-11-19 17:19:18 +0530

    Added Tutorial

--- app/src/main/java/com/techiespace/projects/hark/MainActivity.java
+++ app/src/main/java/com/techiespace/projects/hark/MainActivity.java
@@ -36,8 +36,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.action_settings: {
+            /*case R.id.action_settings: {
                 Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                break;
+            }*/
+            case R.id.action_tutorial: {
+                Intent intent = new Intent(this, TutorialActivity.class);
                 startActivity(intent);
                 break;
             }
